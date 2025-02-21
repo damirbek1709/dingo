e6f14da8c03cef7a6163e302b33a15f560abaa48
damirbek1709 <damirbek@gmail.com>
2025-02-21 17:27:17 +0600

    updates

--- modules/api/controllers/ObjectController.php
+++ modules/api/controllers/ObjectController.php
@@ -373,7 +373,7 @@ class ObjectController extends BaseController
         // Determine which price field to sort by based on guest amount
         $priceField = 'rooms.tariff.prices.price_' . $guestAmount;
 
-        $pageSize = 1; // Number of results per page
+        $pageSize = 10; // Number of results per page
         $page = (int)Yii::$app->request->get('page', 1); // Get page from request
         $offset = ($page - 1) * $pageSize;
 
@@ -429,6 +429,7 @@ class ObjectController extends BaseController
             'totalCount' => $totalCount,
             'page' => (int) $page,
             'data' => $hits,
+            'offset'=>$offset
         ];
 
         return $arr;
