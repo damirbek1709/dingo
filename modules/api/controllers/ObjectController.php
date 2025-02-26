7776fcb5ef9befb63f7b0072a0b8ddd3c1663a77
damirbek1709 <damirbek@gmail.com>
2025-02-26 12:51:47 +0600

    updates

--- modules/api/controllers/ObjectController.php
+++ modules/api/controllers/ObjectController.php
@@ -565,7 +565,7 @@ class ObjectController extends BaseController
             $user = User::find()->where(['auth_key' => $user_auth])->one();
             $user_search_data = unserialize($user->search_data);
         }
-        $results['user_search_data'] = $user_search_data;
+        $results['user_search_data'] = array_values($user_search_data);
         return $results;
     }
 
