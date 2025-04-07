e8c17e630026c01f18fd10086736f588d8dc6265
damirbek1709 <damirbek@gmail.com>
2025-04-07 13:56:39 +0600

    asd

--- models/user/RegistrationForm.php
+++ models/user/RegistrationForm.php
@@ -37,7 +37,39 @@ class RegistrationForm extends BaseRegistrationForm
     /**
      * @inheritdoc
      */
-    
+
+    public function rules()
+    {
+        $user = $this->module->modelMap['User'];
+
+        return [
+            // username rules
+            'usernameTrim' => ['username', 'trim'],
+            'usernameLength' => ['username', 'string', 'min' => 3, 'max' => 255],
+            'usernamePattern' => ['username', 'match', 'pattern' => $user::$usernameRegexp],
+            'usernameSafe' => ['username', 'safe'],
+            'usernameUnique' => [
+                'username',
+                'unique',
+                'targetClass' => $user,
+                'message' => Yii::t('user', 'This username has already been taken')
+            ],
+            // email rules
+            'emailTrim' => ['email', 'trim'],
+            'emailRequired' => ['email', 'required'],
+            'emailPattern' => ['email', 'email'],
+            'emailUnique' => [
+                'email',
+                'unique',
+                'targetClass' => $user,
+                'message' => Yii::t('user', 'This email address has already been taken')
+            ],
+            // password rules
+            'passwordRequired' => ['password', 'required', 'skipOnEmpty' => $this->module->enableGeneratingPassword],
+            'passwordLength' => ['password', 'string', 'min' => 6, 'max' => 72],
+        ];
+    }
+
 
     /**
      * @inheritdoc
