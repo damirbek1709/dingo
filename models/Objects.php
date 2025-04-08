840499b67a85d3a12e8b80da031471a02f5f9327
damirbek1709 <damirbek@gmail.com>
2025-04-08 18:31:40 +0600

    asd

--- models/Objects.php
+++ models/Objects.php
@@ -74,7 +74,7 @@ class Objects extends \yii\db\ActiveRecord {
     const OBJECT_TYPE_GUESTHOUSE = 8;
     const OBJECT_TYPE_RESIDENTIAL_PREMISES = 9;
 
-    public $primaryKey = 'link_id'; 
+    
 
     public function rules()
     {
