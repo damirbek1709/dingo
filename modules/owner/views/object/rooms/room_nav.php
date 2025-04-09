33d7d69867101b44dd441d59e5000488a41c62e4
damirbek1709 <damirbek@gmail.com>
2025-04-09 18:07:21 +0600

    asd

--- modules/owner/views/object/rooms/room_nav.php
+++ modules/owner/views/object/rooms/room_nav.php
@@ -5,7 +5,7 @@ use yii\helpers\Html;
 <div class="owner-nav-cover row">
 
     <div class="owner-nav-item">
-        <?= Html::a(Yii::t('app', 'Информация'), ['room', 'id' => $model->id, 'object_id'=>$object_id]); ?>
+        <?= Html::a(Yii::t('app', 'Информация'), ['room', 'id' => $model['id'], 'object_id'=>$object_id]); ?>
     </div>
     <div class="owner-nav-item">
         <?= Html::a(Yii::t('app', 'Услуги и особенности'), ['room-comfort', 'id' => $model->id, 'object_id'=>$object_id]); ?>
