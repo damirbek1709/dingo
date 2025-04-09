59549ba15098ffd7d24597f77b0cfca89711f642
damirbek1709 <damirbek@gmail.com>
2025-04-09 18:14:21 +0600

    asd

--- modules/owner/views/object/rooms/comfort.php
+++ modules/owner/views/object/rooms/comfort.php
@@ -22,7 +22,7 @@ $this->params['breadcrumbs'][] = $this->title;
         <div class="row">
 
             <div class="col-md-3">
-                <?php echo $this->render('room_nav', ['room_id' => $room_id,'object_id'=>$object_id]); ?>
+                <?php echo $this->render('room_nav', ['room_id' => $room['id'],'object_id'=>$object_id]); ?>
             </div>
 
             <div class="col-md-9">
