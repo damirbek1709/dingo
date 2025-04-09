05d0b3504b75c504c7cbf29f0d0416923ad4e515
damirbek1709 <damirbek@gmail.com>
2025-04-09 17:46:14 +0600

    asd

--- modules/owner/views/object/rooms/update.php
+++ modules/owner/views/object/rooms/update.php
@@ -6,7 +6,7 @@ use yii\helpers\Html;
 /** @var app\models\RoomCat $model */
 
 $this->title = Yii::t('app', 'Update Room Category') . " " . $model->room_title;
-$this->params['breadcrumbs'][] = ['label' => $object_title, 'url' => ['room', 'id' => $model->id]];
+$this->params['breadcrumbs'][] = ['label' => $object_title, 'url' => ['object/view', 'id' => $model->id,'object_id'=>$object_id]];
 $this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Список номеров'), 'url' => ['room-list', 'id' => $object_id]];
 
 $this->params['breadcrumbs'][] = ['label' => $model->room_title, 'url' => ['room', 'id' => $model->id,'object_id'=>$object_id]];
