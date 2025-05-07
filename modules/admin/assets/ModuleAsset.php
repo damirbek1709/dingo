dcae385532c4c32ff8509e4c0354cb04e4ceaf9c
damirbek1709 <damirbek@gmail.com>
2025-05-07 17:14:10 +0600

    asd

--- modules/admin/assets/ModuleAsset.php
+++ modules/admin/assets/ModuleAsset.php
@@ -20,7 +20,7 @@ class ModuleAsset extends AssetBundle
     public $sourcePath = '@app/modules/admin/assets';
     public $baseUrl = '@web';
     public $css = [
-        'css/main2.css',
+        'css/main3.css',
         'css/navbar.css',
         'css/room.css',
         'css/dingo.css',
