d4f23ed2c07caf461f9be4c61e2e1553a43cd640
damirbek1709 <damirbek@gmail.com>
2025-05-16 18:30:29 +0600

    flashpay

--- config/db.php
+++ config/db.php
@@ -1,16 +1,16 @@
 <?php
 
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=dingo_dev',
-    'username' => 'dingo_dev',
-    'password' => 'ctpwt4ZpXSsea[(k',
-    'charset' => 'utf8mb4',
-    // Schema cache options (for production environment)
-    // 'enableSchemaCache' => true,
-    // 'schemaCacheDuration' => 60,
-    // 'schemaCache' => 'cache',
-];
+// return [
+//     'class' => 'yii\db\Connection',
+//     'dsn' => 'mysql:host=localhost;dbname=dingo_dev',
+//     'username' => 'dingo_dev',
+//     'password' => 'ctpwt4ZpXSsea[(k',
+//     'charset' => 'utf8mb4',
+//     // Schema cache options (for production environment)
+//     // 'enableSchemaCache' => true,
+//     // 'schemaCacheDuration' => 60,
+//     // 'schemaCache' => 'cache',
+// ];
 
 // return [
 //     'class' => 'yii\db\Connection',
