499f7caa8cecbd702aee6e2ff83f813b895390ce
damirbek1709 <damirbek@gmail.com>
2025-05-19 11:58:08 +0600

    qwer

--- models/Objects.php
+++ models/Objects.php
@@ -413,8 +413,8 @@ class Objects extends \yii\db\ActiveRecord {
                 'options' => [
                     'class' => 'owner-nav-item-object',
                 ],    
-            ];   
-        } 
+            ];    
+        }
             
         return $general_arr;
     }
