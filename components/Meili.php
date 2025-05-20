df4854e615d0f444f8beb66c04986011ba905628
damirbek1709 <damirbek@gmail.com>
2025-05-20 13:24:36 +0600

    qwer

--- components/Meili.php
+++ components/Meili.php
@@ -33,11 +33,12 @@ class Meili extends Component
     public function connect($content = null)
     {
         $server = strtolower($_SERVER['HTTP_HOST'] ?? php_uname('n'));
+
         try {
             if ($server === 'partner.dingo.kg') {
                 $client = new Client(
                     'https://ms.dingo.kg',
-                    'e4ed3a1359dc6afb1760a335f7559f0373e74ea9df88c1a7ebe1f3e14150592a',
+                    'XYgN9akmwIizAMP6Z1zRAE8qryqcjZY1XQBT-qLUG3g',
                     new GuzzleHttpClient([
                         'timeout' => 5,
                         'verify' => false
