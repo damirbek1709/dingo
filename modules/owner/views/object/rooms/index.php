1fd11b203e402d7ef92f6a90aa619cc36f95843e
damirbek1709 <damirbek@gmail.com>
2025-05-21 18:57:39 +0600

    lorem

--- modules/owner/views/object/rooms/index.php
+++ modules/owner/views/object/rooms/index.php
@@ -20,7 +20,7 @@ $this->title = Yii::t('app', 'Список номеров');
 <link href="<?= Url::base() ?>/modules/owner/assets/css/room.css" rel="stylesheet">
 <div class="oblast-update">
     <!-- <h1><?php //echo Html::encode($object_title) ?></h1> -->
-    <?php echo $this->render('../top_nav', ['model' => $model,'object_id'=>$object_id]); ?>
+    <?php echo $this->render('../top_nav', ['model' => $model, 'object_id' => $object_id]); ?>
 
     <div class="clear">
         <div class="card">
@@ -46,16 +46,17 @@ $this->title = Yii::t('app', 'Список номеров');
                 ?>
                 <div class="room_list clear">
                     <div class="room-card">
-                        <?php 
-                        if($bind_model){
-                            echo Html::a(Html::img($bind_model->getImage()->getUrl('120x150'), ['alt' => "Room Image"]), ['/owner/object/room', 'id' => $room_id, 'object_id' => $object_id]); 
-                        
-                        }
-                        else{
-                            echo Html::a($val['images']['picture'], ['/owner/object/room', 'id' => $room_id, 'object_id' => $object_id]); 
-                        
-                        }?>
-                       <div class="room-card-details">
+                        <?php
+                        if ($bind_model) {
+                            echo Html::a(Html::img($bind_model->getImage()->getUrl('120x150'), ['alt' => "Room Image"]), ['/owner/object/room', 'id' => $room_id, 'object_id' => $object_id]);
+
+                        } else {
+                            if ($val['images']) {
+                                echo Html::a($val['images']['picture'], ['/owner/object/room', 'id' => $room_id, 'object_id' => $object_id]);
+                            }
+
+                        } ?>
+                        <div class="room-card-details">
                             <h3><?= Html::a($val['room_title'][0], ['/owner/object/room', 'id' => $room_id, 'object_id' => $object_id]); ?>
                             </h3>
                             <div class="room-info">
