aaeff0f56b752b1ee641a1eb34cd6666253f972f
damirbek1709 <damirbek@gmail.com>
2025-05-28 20:16:43 +0600

    trew

--- modules/admin/views/object/top_nav.php
+++ modules/admin/views/object/top_nav.php
@@ -12,10 +12,7 @@ use yii\helpers\Html;
     $class = "style_" . $action;
     ?>
     <?= Html::a('Отель', ['view', 'object_id' => $object_id], ['class' => "style_hotel top_nav_title top_nav_title_hotel"]); ?>
-    <?php
-    if (Yii::$app->user->can('owner'))
-        echo Html::a('Доступность и цены', ['prices', 'object_id' => $object_id], ['class' => "style_prices top_nav_title top_nav_title_prices"]); ?>
-
+    
     <?php echo Html::a('Бронирования', ['booking', 'object_id' => $object_id], ['class' => 'style_booking top_nav_title top_nav_title_booking']); ?>
     <?php echo Html::a('Номера и тарифы', ['room-list', 'object_id' => $object_id], ['class' => 'style_rooms style_room-list top_nav_title top_nav_title_rooms']); ?>
     <?php //echo Html::a('Финансы', ['finances', 'object_id' => $object_id], ['class' => 'style_finance top_nav_title top_nav_title_finance']); ?>
