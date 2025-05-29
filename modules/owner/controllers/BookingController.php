9392fed905a8092c0f85df30ab767d40fb6afdd6
damirbek1709 <damirbek@gmail.com>
2025-05-29 12:35:38 +0600

    trew

--- modules/owner/controllers/BookingController.php
+++ modules/owner/controllers/BookingController.php
@@ -91,6 +91,7 @@ class BookingController extends Controller
                 case "canceled":
                     $dataProvider->query->andFilterWhere(['status' => Booking::PAID_STATUS_CANCELED]);
                     $active = "cancel_active";
+                    echo "<pre>";print_r($dataProvider->models);echo "</pre>";die();
                     break;
                 default:
                     $dataProvider->query->andFilterWhere(['object_id' => $object_id]);
