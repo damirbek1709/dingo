af4402acce7d33f8b3467266876c51b46231a65c
damirbek1709 <damirbek@gmail.com>
2025-07-02 19:20:25 +0600

    qwerty

--- modules/owner/views/object/create.php
+++ modules/owner/views/object/create.php
@@ -17,6 +17,7 @@ $this->title = Yii::t('app', 'Создание объекта');
                 <h1><?= Html::encode($this->title) ?></h1>
                 <?= $this->render('_form', [
                     'model' => $model,
+                    'initCityText'=>$initCityText
                 ]) ?>
             </div>
         </div>
