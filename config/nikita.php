6e12c68450baaf53dcb2114274365d73536d2567
damirbek1709 <damirbek@gmail.com>
2025-07-16 13:14:30 +0600

    treqw

--- config/nikita.php
+++ config/nikita.php
@@ -2,11 +2,11 @@
 
 return [
     'class' => 'app\components\Nikita',
-    // 'login' => 'sydykovm',
-    // 'password' => 'kUbTBg7C',
-    // 'sender' => 'SMSPRO.KG',
+    'login' => 'sydykovm',
+    'password' => 'kUbTBg7C',
+    'sender' => 'SMSPRO.KG',
 
-    'login' => 'Selva',
-    'password' => '33xmJ_Xy',
-    'sender' => 'Selva.kg',
+    // 'login' => 'Selva',
+    // 'password' => '33xmJ_Xy',
+    // 'sender' => 'Selva.kg',
 ];
