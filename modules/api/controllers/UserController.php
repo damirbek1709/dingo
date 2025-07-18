0d3b3d4074673bf8de71367c10d3c5e2ed36600b
damirbek1709 <damirbek@gmail.com>
2025-07-18 14:11:33 +0600

    treqw

--- modules/api/controllers/UserController.php
+++ modules/api/controllers/UserController.php
@@ -177,7 +177,7 @@ class UserController extends BaseController
         $ids = array_values($fav_arr);
         $totalCount = count($ids);
         $pageSize = 10;
-        $page = max(1, (int) Yii::$app->request->get('page', 1));
+        $page = (int) Yii::$app->request->get('page', 1);
         $offset = ($page - 1) * $pageSize;
 
         if (empty($ids) || $offset >= $totalCount) {
