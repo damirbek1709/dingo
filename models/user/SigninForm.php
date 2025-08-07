15d492b526458168d66beb096f02cd7fb74a559e
damirbek1709 <damirbek@gmail.com>
2025-08-08 01:32:38 +0600

    qwerty

--- models/user/SigninForm.php
+++ models/user/SigninForm.php
@@ -44,6 +44,7 @@ class SigninForm extends \yii\base\Model
     {
         return [
             'email' => 'E-mail',
+            'phone' => Yii::t('app','Телефон'),
         ];
     }
 
@@ -51,52 +52,5 @@ class SigninForm extends \yii\base\Model
      *
      * @return type
      */
-    public function signin()
-    {
-        if ($this->validate()) {
-            $user = User::find()->where(['email' => $this->email])->one();
-            if ($user === null) {
-                $model = new RegistrationForm();
-                $model->username = $this->email;
-                $model->email = $this->email;
-                $model->phone = $this->phone;
-
-                if ($model->validate() && $model->register()) {
-                    return true;
-                } else {
-                    $this->addError('email', $model->getFirstError('username'));
-                }
-            } else {
-                //$user->confirmed_at = null;
-                $user->save();
-                $sendSMS = true;
-
-                if (in_array($user->username, ['damirbek@gmail.com'])) {
-                    $dao = Yii::$app->db;
-                    $dao->createCommand()->delete('token', ['user_id' => $user->id])->execute();
-                    $dao->createCommand()->insert('token', ['user_id' => $user->id, 'code' => '0000', 'type' => Token::TYPE_CONFIRMATION, 'created_at' => time()])->execute();
-                    $sendSMS = false;
-                } else {
-                    $token = \Yii::createObject(['class' => Token::className(), 'type' => Token::TYPE_CONFIRMATION]);
-                    $token->link('user', $user);
-                }
-
-                $recipient = '+' . $this->phone;
-                if ($sendSMS) {
-                    Yii::$app->nikita->setRecipient($recipient)
-                        ->setText('Ваш код: ' . $token->code . ' is your code' . PHP_EOL . 'wYvKRPwmEXI')
-                        ->send();
-                    // Yii::$app->mailer->compose()
-                    //     ->setFrom('send@dingo.kg')
-                    //     ->setTo($this->email)
-                    //     ->setSubject("Ваш код авторизации: " . $token->code)
-                    //     ->setHtmlBody("<h1>{$token->code}</h1>")
-                    //     ->setTextBody('Hello from Resend! This is a test email.')
-                    //     ->send();
-                }
-
-                return true;
-            }
-        }
-    }
+    
 }
